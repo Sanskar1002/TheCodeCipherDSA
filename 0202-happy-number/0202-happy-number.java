3d53485cc0610b691e87f967efdd3c9b72226d46
Sanskar Wankhede <115367384+Sanskar1002@users.noreply.github.com>
2026-03-19 23:23:36 +0530

    Time: 1 ms (77.56%), Space: 42.7 MB (30.25%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int getNextNumber(int n){
+        int output = 0;
+        while(n!=0){
+            int digit = n%10;
+            output +=digit*digit;
+            n = n/10;
+        }
+
+        return output;
+    }
+    public boolean isHappy(int n) {
+        Set<Integer> visit = new HashSet<>();
+        while(!visit.contains(n)){ // jab tak set me vo element nhi mila (repeat nhi hua)
+            visit.add(n);
+            n = getNextNumber(n);
+            if(n==1){
+                return true;
+            }
+        };
+
+        return false;
+        
+    }
+}
