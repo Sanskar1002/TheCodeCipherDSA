802d5c265d6d0738d400229a9a5d60cbae4f641d
Sanskar Wankhede <115367384+Sanskar1002@users.noreply.github.com>
2026-05-23 15:40:16 +0530

    Time: 0 ms (100%), Space: 44.7 MB (47.16%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length-1;
+
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return lo;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
