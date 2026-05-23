03d860553eb2c8d19ff314f96764d7676f794ad3
Sanskar Wankhede <115367384+Sanskar1002@users.noreply.github.com>
2026-05-23 15:26:19 +0530

    Time: 0 ms (100%), Space: 31.4 MB (44.97%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size()-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid]== target){
+                return mid;
+            }else if(nums[mid]<target){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
