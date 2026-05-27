691b7d6d0722caabf990fe2a79f1f6ae78827f02
Sanskar Wankhede <115367384+Sanskar1002@users.noreply.github.com>
2026-05-28 00:11:39 +0530

    Time: 2 ms (15.67%), Space: 8.8 MB (88.26%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char br:s){
+            if(br == '(' || br=='[' || br == '{'){
+                st.push(br);
+            }else{
+                if(st.empty()) return false;
+                char top = st.top();
+                if((top == '(' && br != ')') || (top == '[' && br != ']') || (top == '{' && br != '}')) return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
